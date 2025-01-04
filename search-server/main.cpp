efcd83e0cafff401f35a89854d5155a81d80dfd2
sxyqww <zamotaev-03@mail.ru>
2025-01-04 20:00:12 +0300

    Added a parallel function that parallelizes the processing of multiple search engine requests

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -4,7 +4,7 @@
 #include "paginator.h"
 #include "log_duration.h"
 #include "remove_duplicates.h"
-
+#include "process_queries.h"
 
 void AddDocument(SearchServer& search_server, int id, std::string document, DocumentStatus status, std::vector<int> ratings) { 
     search_server.AddDocument(id, document, status, ratings); 
@@ -13,32 +13,25 @@ void AddDocument(SearchServer& search_server, int id, std::string document, Docu
 int main() {
     using namespace std;
     SearchServer search_server("and with"s);
-
-    AddDocument(search_server, 1, "funny pet and nasty rat"s, DocumentStatus::ACTUAL, {7, 2, 7});
-    AddDocument(search_server, 2, "funny pet with curly hair"s, DocumentStatus::ACTUAL, {1, 2});
-
-    // дубликат документа 2, будет удалён
-    AddDocument(search_server, 3, "funny pet with curly hair"s, DocumentStatus::ACTUAL, {1, 2});
-
-    // отличие только в стоп-словах, считаем дубликатом
-    AddDocument(search_server, 4, "funny pet and curly hair"s, DocumentStatus::ACTUAL, {1, 2});
-
-    // множество слов такое же, считаем дубликатом документа 1
-    AddDocument(search_server, 5, "funny funny pet and nasty nasty rat"s, DocumentStatus::ACTUAL, {1, 2});
-
-    // добавились новые слова, дубликатом не является
-    AddDocument(search_server, 6, "funny pet and not very nasty rat"s, DocumentStatus::ACTUAL, {1, 2});
-
-    // множество слов такое же, как в id 6, несмотря на другой порядок, считаем дубликатом
-    AddDocument(search_server, 7, "very nasty rat and not very funny pet"s, DocumentStatus::ACTUAL, {1, 2});
-
-    // есть не все слова, не является дубликатом
-    AddDocument(search_server, 8, "pet with rat and rat and rat"s, DocumentStatus::ACTUAL, {1, 2});
-
-    // слова из разных документов, не является дубликатом
-    AddDocument(search_server, 9, "nasty rat with curly hair"s, DocumentStatus::ACTUAL, {1, 2});
-    
-    cout << "Before duplicates removed: "s << search_server.GetDocumentCount() << endl;
-    RemoveDuplicates(search_server);
-    cout << "After duplicates removed: "s << search_server.GetDocumentCount() << endl;
-}
+    int id = 0;
+    for (
+        const string& text : {
+            "funny pet and nasty rat"s,
+            "funny pet with curly hair"s,
+            "funny pet and not very nasty rat"s,
+            "pet with rat and rat and rat"s,
+            "nasty rat with curly hair"s,
+        }
+    ) {
+        search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, {1, 2});
+    }
+    const vector<string> queries = {
+        "nasty rat -not"s,
+        "not very funny nasty pet"s,
+        "curly hair"s
+    };
+    for (const Document& document : ProcessQueriesJoined(search_server, queries)) {
+        cout << "Document "s << document.id << " matched with relevance "s << document.relevance << endl;
+    }
+    return 0;
+} 
---
+++ search-server/process_queries.cpp
@@ -0,0 +1,28 @@
+#include "process_queries.h"
+
+using std::vector;
+
+
+vector<vector<Document>> ProcessQueries(
+    const SearchServer& search_server,
+    const vector<std::string>& queries) {
+        vector<vector<Document>> res(queries.size());
+        std::transform(queries.begin(), queries.end(), res.begin(), 
+        [&search_server] (const std::string& query) {
+            return search_server.FindTopDocuments(query);
+        }
+        );
+    return res; 
+    }
+
+std::list<Document> ProcessQueriesJoined(
+    const SearchServer& search_server,
+    const vector<std::string>& queries) {
+        vector<vector<Document>> partial_result = ProcessQueries(search_server, queries);
+        std::list<Document> joined_result;
+        for (const auto& vec_doc: partial_result) {
+            joined_result.insert(joined_result.end(), vec_doc.begin(), vec_doc.end());
+        }
+
+        return joined_result;
+    }
